8ba898180f1a040ab5d7fe29d269b175825976ba
Alex Doehring <alex.doehring2@gmail.com>
2025-04-04 21:59:58 -0500

    Updated deck.py

--- src/deck.py
+++ src/deck.py
@@ -7,26 +7,28 @@ class Deck:
         ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
         suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
-        self.discard_pile = []
-
-    def deal(self, num_cards=1):
-        if len(self.cards) < num_cards:
-            self.reshuffle()
-        dealt_cards = []
-        for i in range(num_cards):
-            card = self.cards.pop()
-            dealt_cards.append(card)
-        return dealt_cards
+        self.drawn_cards = []
 
     def reshuffle(self):
-        while len(self.discard_pile) > 1:
-            temp = self.discard_pile.pop()
+        while len(self.drawn_cards) > 1:
+            temp = self.drawn_cards.pop()
             self.cards.append(temp)
         random.shuffle(self.cards)
 
-    def draw(self):
+    def draw(self, card=None):
         if len(self.cards) < 1:
             self.reshuffle()
+            
+        # If a specific card is requested, check if it's in the deck
+        # and move it to drawn cards if it is.    
+        if card is not None:
+            if card in self.cards:
+                self.cards.remove(card)
+                self.drawn_cards.append(card)
+                return card
+            else:
+                raise ValueError("Card not found in the deck.")
+        # If no specific card is requested, draw the top card
         value = self.cards.pop()
         return value
 
