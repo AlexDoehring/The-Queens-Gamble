a11b6bc2822c31d09566c5794aaeff196093292c
Alex Doehring <alex.doehring2@gmail.com>
2025-04-04 23:10:55 -0500

    Added bounties to each piece

--- src/piece.py
+++ src/piece.py
@@ -2,12 +2,13 @@ import os
 
 class Piece:
 
-    def __init__(self, name, color, value, texture=None, texture_rect=None):
+    def __init__(self, name, color, value, bounty=0, texture=None, texture_rect=None):
         self.name = name
         self.color = color
         value_sign = 1 if color == 'white' else -1
         self.value = value * value_sign
         self.moves = []
+        self.bounty = bounty
         self.moved = False
         self.texture = texture
         self.set_texture()
@@ -28,27 +29,27 @@ class Pawn(Piece):
     def __init__(self, color):
         self.dir = -1 if color == 'white' else 1
         self.en_passant = False
-        super().__init__('pawn', color, 1.0)
+        super().__init__('pawn', color, 1.0, 2.0)
 
 class Knight(Piece):
 
     def __init__(self, color):
-        super().__init__('knight', color, 3.0)
+        super().__init__('knight', color, 3.0, 5.0)
 
 class Bishop(Piece):
 
     def __init__(self, color):
-        super().__init__('bishop', color, 3.001)
+        super().__init__('bishop', color, 3.001, 5.0)
 
 class Rook(Piece):
 
     def __init__(self, color):
-        super().__init__('rook', color, 5.0)
+        super().__init__('rook', color, 5.0, 10.0)
 
 class Queen(Piece):
 
     def __init__(self, color):
-        super().__init__('queen', color, 9.0)
+        super().__init__('queen', color, 9.0, 20.0)
 
 class King(Piece):
 
