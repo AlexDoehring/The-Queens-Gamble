c2a54a404ce0b766466761d6abbd21ec23db0889
Alex Doehring <alex.doehring2@gmail.com>
2025-04-04 22:57:24 -0500

    Updated rank return

--- src/card.py
+++ src/card.py
@@ -7,7 +7,35 @@ class Card:
         return self._suit
 
     def getRank(self):
-        return self._rank
+        match self._rank:
+            case "Ace":
+                return 1
+            case "Two":
+                return 2
+            case "Three":
+                return 3
+            case "Four":
+                return 4
+            case "Five":
+                return 5
+            case "Six":
+                return 6
+            case "Seven":
+                return 7
+            case "Eight":
+                return 8
+            case "Nine":
+                return 9
+            case "Ten":
+                return 10
+            case "Jack":
+                return 10
+            case "Queen":
+                return 10
+            case "King":
+                return 10
+            case _:
+                raise ValueError("Invalid rank")
 
     def __lt__(self, other):
         return (self._rank < other._rank)
